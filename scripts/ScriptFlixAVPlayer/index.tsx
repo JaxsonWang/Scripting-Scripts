74191e0d0a50bec0d39db92acc10210a6e972451
Jaxson Wang <i@iiong.com>
2026-02-28 14:07:17 +0800

    Remove ScriptFlixAVPlayer module and associated files

--- scripts/ScriptFlixAVPlayer/index.tsx
+++
@@ -1,9 +0,0 @@
-import { Navigation, Script } from 'scripting'
-import { HomeScreen } from './screens/HomeScreen'
-
-const run = async () => {
-  await Navigation.present({ element: <HomeScreen />, modalPresentationStyle: 'fullScreen' })
-  Script.exit()
-}
-
-run()
